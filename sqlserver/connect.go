65b61a88dd7d2dcba4ab3a511b982cc6908b23b5
carlca <carl.caulkett@gmail.com>
2017-12-03 23:37:55 +0000

    Tidied up ffor Daniel to look at

--- sqlserver/connect.go
+++ sqlserver/connect.go
@@ -4,8 +4,8 @@ import (
 	"database/sql"
 	"flag"
 	"fmt"
-	"log"
-	// _ "github.com/denisenkom/go-mssqldb"
+
+	e "github.com/carlca/utils/essentials"
 )
 
 var (
@@ -16,28 +16,81 @@ var (
 	user     = flag.String("user", "", "the database user")
 )
 
-// Connect established contact with an SQL Server
-func Connect() *sql.DB {
+// DB inherits from sql.DB
+type DB struct {
+	*sql.DB
+}
+
+// Exec combines Prepare and Exec methods
+// func (db *DB) Exec(cmd string) {
+// 	stmt, err := db.Prepare(cmd)
+// 	e.CheckError("prepare: "+cmd+" failed", err)
+// 	_, err = stmt.Exec()
+// 	e.CheckError("exec: "+cmd+" failed", err)
+// 	if *debug {
+// 		fmt.Printf("exec: %v succeeded\n", cmd)
+// 	}
+// }
+
+// NewTx wraps the *DB.Begin func
+func (db *DB) NewTx() *Tx {
+	tnx, err := db.Begin()
+	e.CheckError("BeginTx failed: ", err)
+	if *debug {
+		fmt.Printf("NewTx: succeeded\n")
+	}
+	return &Tx{*tnx}
+}
+
+// Tx inherits from sql.Tx
+type Tx struct {
+	sql.Tx
+}
+
+// Exec wraps *Tx.Exec
+func (tx *Tx) Exec(cmd string) {
+	stmt, err := tx.Prepare(cmd)
+	e.CheckError("prepare: "+cmd+" failed", err)
+	_, err = stmt.Exec()
+	e.CheckError("exec: "+cmd+" failed", err)
+	if *debug {
+		fmt.Printf("exec: %v succeeded\n", cmd)
+	}
+}
+
+// CommitTx wraps the *Tx.Commit func
+func (tx *Tx) CommitTx() {
+	err := tx.Commit()
+	e.CheckError("tx.CommitTx failed: ", err)
+	if *debug {
+		fmt.Printf("tx.CommitTx succeeded\n")
+	}
+}
+
+// Connect establishes contact with an SQL Server
+func Connect() *DB {
 	// parse command line flags
 	flag.Parse()
 	// dump flags if debug
 	if *debug {
-		fmt.Printf(" password:%s\n", *password)
-		fmt.Printf(" port:%d\n", *port)
-		fmt.Printf(" server:%s\n", *server)
-		fmt.Printf(" user:%s\n", *user)
+		fmt.Printf("password: %s\n", *password)
+		fmt.Printf("port: %d\n", *port)
+		fmt.Printf("server: %s\n", *server)
+		fmt.Printf("user: %s\n", *user)
 	}
 	// build connection string
 	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d", *server, *user, *password, *port)
 	// if debug dump connection string
 	if *debug {
-		fmt.Printf(" connString:%s\n", connString)
+		fmt.Printf("connString: %s\n", connString)
 	}
 	// create an SQL Server connection
-	conn, err := sql.Open("mssql", connString)
-	if err != nil {
-		log.Fatal("Open connection failed:", err.Error())
+	dbx, err := sql.Open("mssql", connString)
+	e.CheckError("Open DB failed: ", err)
+	if *debug {
+		fmt.Printf("open mssql: succeeded\n")
 	}
-	defer conn.Close()
-	return conn
+	err = dbx.Ping()
+	e.CheckError("db.Ping failed", err)
+	return &DB{dbx}
 }
