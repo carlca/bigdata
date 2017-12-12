27d546fc4827e6e3bbc95b0f00cff74141f7830d
carlca <carl.caulkett@gmail.com>
2017-12-12 22:54:47 +0000

    getting github up tp date

--- postgresql/connect.go
+++ postgresql/connect.go
@@ -1,4 +1,4 @@
-package sqlserver
+package postgresql
 
 import (
 	"database/sql"
@@ -6,16 +6,15 @@ import (
 	"fmt"
 
 	e "github.com/carlca/utils/essentials"
-	// MSSQL driver
-	_ "github.com/denisenkom/go-mssqldb"
+	// PostgreSQL driver
+	_ "github.com/lib/pq"
 )
 
 var (
 	debug    = flag.Bool("debug", false, "enable debugging")
-	password = flag.String("password", "", "the database password")
-	port     = flag.Int("port", 1433, "the database port")
-	server   = flag.String("server", "", "the database server")
 	user     = flag.String("user", "", "the database user")
+	dbname   = flag.String("dbname", "", "the database name")
+	password = flag.String("password", "", "the database password")
 )
 
 // DB inherits from sql.DB
@@ -29,19 +28,19 @@ func Connect() (*DB, bool) {
 	flag.Parse()
 	// dump flags if debug
 	if *debug {
-		fmt.Printf("password: %s\n", *password)
-		fmt.Printf("port: %d\n", *port)
-		fmt.Printf("server: %s\n", *server)
 		fmt.Printf("user: %s\n", *user)
+		fmt.Printf("dbname: %s\n", *dbname)
+		fmt.Printf("password: %s\n", *password)
 	}
 	// build connection string
-	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d", *server, *user, *password, *port)
+	connString := fmt.Sprintf("user=%s dbname=%s password=%s", *user, *dbname, *password)
+	connString = connString + " sslmode=verify-full"
 	// if debug dump connection string
 	if *debug {
 		fmt.Printf("connString: %s\n", connString)
 	}
 	// create an SQL Server connection
-	dbx, err := sql.Open("mssql", connString)
+	dbx, err := sql.Open("postgres", connString)
 	e.CheckError("Open DB", err, *debug)
 	err = dbx.Ping()
 	e.CheckError("db.Ping", err, *debug)
